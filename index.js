024c00bdd6c0ab26a7ec407673a76a11e8ac83fc
Varsani2520 <varsaniranjani2520@gmail.com>
2024-02-07 11:10:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const multer = require("multer");
+
+const app = express();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./upload");
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+const upload = multer({ storage });
+app.set("view engine", "ejs");
+app.set("views ", path.resolve("./views"));
+
+// middleware
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  return res.render("homepage");
+});
+
+app.post("/upload", upload.single("profileImage"), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  return res.redirect("/");
+});
+app.listen(8000, () => console.log("server started"));
